56da5f69783de54c8da9f772462b9e0323ff7bb3
jandib <142757988+jjjandib6013@users.noreply.github.com>
2024-11-25 22:12:46 +0800

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -17,8 +17,32 @@ public class Appointment implements Serializable {
         this.location = location;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
     @Override
     public String toString() {
-            return "Title: " + title + "\nName: " + name + "\nDate: " + date + "\nTime: " + time + "\nDescription: " + description + "\nLocation: " + location + "\n";
+        return "Title: " + title + "\nName: " + name + "\nDate: " + date + "\nTime: " + time + "\nDescription: " + description + "\nLocation: " + location + "\n";
     }
 }
